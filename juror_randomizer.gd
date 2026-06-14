1bfee92eced79c15801ab73dc88ec4bcadf16b16
CashRed <crane1dog@gmail.com>
2026-06-13 19:34:43 -0700

    Mac upd, visual brainstorm
    yo stop listeeing to gemini bc you do peak work man

---
+++ juror_randomizer.gd
@@ -0,0 +1,4 @@
+extends Node
+
+@export var juror_spawn Array[PackedScene]
+@export var juror_spawn_rates: Array[float]
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,11 +1,12 @@
 extends Control
 const GAMEPLAY_SCENE = "res://scenes/gameplay.tscn" #makes a path to the game's scene
+const Settings_Scene = "res://scenes/settings_menu.tscn"
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file(GAMEPLAY_SCENE)
 
-func _on_quit_button_pressed() -> void:
+func _on_settings_button_pressed() -> void:
 	print("Settings opened")
 
-func _on_settings_button_pressed() -> void:
+func _on_quit_button_pressed() -> void:
 	get_tree().quit()
